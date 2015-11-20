3682478c8c864a3f2dc6cf71c788e92a90696f20
Katherine Soohoo <katherinesoohoo@gmail.com>
2015-11-20 13:48:02 -0800

    updated removedOrder() and added javadoc comments

--- src/PickUpList.java
+++ src/PickUpList.java
@@ -3,10 +3,17 @@ import java.util.LinkedList;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+/**
+ * List of orders to pick up.
+ */
 public class PickUpList extends LinkedList<Order>
 {
     ChangeListener listener;
-        
+    
+    /**
+     * Adds order to pick up list.
+     * @param order order to add
+     */
     public void addOrder(Order order)
     {
         if (order != null)
@@ -16,22 +23,34 @@ public class PickUpList extends LinkedList<Order>
         }
     }
     
-    public Order removeOrder()
+    /**
+     * Removes order from pick up list.
+     * @param position location of order in list
+     * @return 
+     */
+    public Order removeOrder(int position)
     {
         if (!isEmpty())
         {
-            Order order = remove();
+            Order order = remove(position);
             update();
             return order;
         }
         return null;
     }
     
+    /**
+     * Attaches change listener.
+     * @param c listener
+     */
     public void attach(ChangeListener c)
     {
         listener = c;
     }
     
+    /**
+     * Updates change listener.
+     */
     public void update()
     {
         listener.stateChanged(new ChangeEvent(this));
