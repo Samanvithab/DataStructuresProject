a6b71ab7cb9156d38a23742786c37dbb5137f41c
Katherine Soohoo <katherinesoohoo@gmail.com>
2015-11-22 19:37:31 -0800

    moved hashmap tester here

--- src/MealTester.java
+++ src/MealTester.java
@@ -1,5 +1,6 @@
 
 import datastructures.AVLTree;
+import datastructures.Stack;
 import java.util.Iterator;
 
 public class MealTester
@@ -8,7 +9,10 @@ public class MealTester
     {
         Restaurant restaurant = new Restaurant("Yummy Tummy");
         MainFrame mainFrame = new MainFrame(restaurant);
-        
+    }
+    
+    public void AVLTreeTester()
+    {
         AVLTree tree1 = new AVLTree<Integer>();
         tree1.insert(20);
         tree1.insert(15);
@@ -24,5 +28,31 @@ public class MealTester
         
         while(iter.hasNext())
             System.out.println(iter.next());
+    }
+    
+    public void HashMapTester()
+    {
+        HashMap<Integer, Integer> hashMapCustom = new HashMap<Integer, Integer>();
+        hashMapCustom.put(21, 12);
+        hashMapCustom.put(25, 121);
+        hashMapCustom.put(30, 151);
+        hashMapCustom.put(33, 15);
+        hashMapCustom.put(35, 89);
+
+        System.out.println("value corresponding to key 21="
+                     + hashMapCustom.get(21));
+        System.out.println("value corresponding to key 51="
+                     + hashMapCustom.get(51));
+
+        System.out.print("Displaying : ");
+        hashMapCustom.display();
+
+        System.out.println("\n\nvalue corresponding to key 21 removed: "
+                     + hashMapCustom.remove(21));
+        System.out.println("value corresponding to key 51 removed: "
+                     + hashMapCustom.remove(51));
+
+        System.out.print("Displaying : ");
+        hashMapCustom.display();
     }
 }
