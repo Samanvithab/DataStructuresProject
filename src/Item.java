cf4fa0af7d2364526a37587d98d271b666f40f48
Samanvitha Basole <Samanvithab@users.noreply.github.com>
2015-11-26 12:31:54 -0800

    Added constructor, getters, setters

--- src/Item.java
+++ src/Item.java
@@ -2,5 +2,39 @@ public class Item
 {
     private String name;
     private double price;
-    private int quantity;
+    private int quantity = 0;
+    
+    //constructor
+    public Item(String n, double p, int q) {
+    	name = n;
+    	price = p;
+    	quantity = q;
+    }
+    
+    //getters    
+    public String getName() {
+    	return this.name;
+    }
+    
+    public double getPrice() {
+    	return price;
+    }
+    
+    public int getQuantity() {
+    	return quantity;
+    }
+    
+    //setters
+    public void setName(String n) {
+    	this.name = n;
+    }
+    
+    public void setPrice(double p) {
+    	this.price = p;
+    }
+    
+    public void setQuantity(int q) {
+    	this.quantity = q;
+    }
+    
 }
