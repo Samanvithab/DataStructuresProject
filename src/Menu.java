833034e21d4810a113c8024bf0d519f596dd6527
Katherine Soohoo <katherinesoohoo@gmail.com>
2015-11-27 10:59:35 -0800

    added menu data

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,28 @@
 
 import datastructures.AVLTree;
-import java.util.HashMap;
+import datastructures.HashMap;
 
+/**
+ * A menu with categories and list of items.
+ */
 public class Menu extends HashMap<String, AVLTree>
 {
+    /**
+     * Adds an entry to the menu.
+     * @param category category of items
+     * @param itemList list of items
+     */
+    public void addEntry(String category, AVLTree itemList)
+    {
+        put(category, itemList);  
+    }
     
+    /**
+     * Removes a category and its items from the menu.
+     * @param category category to remove
+     */
+    public void removeEntry(String category)
+    {
+        remove(category);
+    }
 }
