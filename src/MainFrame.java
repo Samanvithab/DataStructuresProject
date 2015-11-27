43c961151321991b732091473d54a7faeef8352b
Katherine Soohoo <katherinesoohoo@gmail.com>
2015-11-27 11:02:48 -0800

    removed refresh methods- MenuFrame switches between panels

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -10,16 +10,13 @@ import javax.swing.JFrame;
  */
 public class MainFrame extends JFrame
 {
-	private Restaurant r;
     private JDesktopPane desktopPane;
-    private OrderFrame orderFrame;
     private MenuFrame menuFrame;
     private KitchenFrame kitchenFrame;
     private PickUpFrame pickUpFrame;
     
-    private static int PANE_WIDTH = 900;
-    private static int PANE_HEIGHT = 500;
-    private Order o = new Order();
+    private static int PANE_WIDTH = 1200;
+    private static int PANE_HEIGHT = 600;
     
     /**
      * Creates the main frame holding internal frames.
@@ -27,7 +24,6 @@ public class MainFrame extends JFrame
      */
     public MainFrame(Restaurant restaurant)
     {
-    	r = restaurant;
         // create desktop pane
         desktopPane = new JDesktopPane();
         desktopPane.addComponentListener(new DesktopPaneListener());
@@ -36,9 +32,7 @@ public class MainFrame extends JFrame
                 new Dimension(PANE_WIDTH, PANE_HEIGHT));
 
         // create internal frames with size and position
-      
-        menuFrame = new MenuFrame(PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, restaurant, this, o);
-        orderFrame = new OrderFrame(PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, restaurant, o);
+        menuFrame = new MenuFrame(PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, restaurant);
         kitchenFrame = new KitchenFrame(
                 PANE_WIDTH / 3, PANE_HEIGHT, PANE_WIDTH / 3, 0, restaurant);
         pickUpFrame = new PickUpFrame(
@@ -48,14 +42,6 @@ public class MainFrame extends JFrame
         restaurant.getKitchen().attach(kitchenFrame);
         restaurant.getPickUpList().attach(pickUpFrame);
         
-        // add internal frames to desktop pane
-        //desktopPane.add(orderFrame);
-        
-        if(menuFrame.getValue()==true) {
-        	desktopPane.remove(menuFrame);
-        	desktopPane.add(orderFrame);
-        }
-        
         desktopPane.add(menuFrame);
         desktopPane.add(kitchenFrame);
         desktopPane.add(pickUpFrame);
@@ -66,89 +52,10 @@ public class MainFrame extends JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-    }  
-    
-    public void refreshMenuFrame() {
-    	desktopPane = new JDesktopPane();
-        desktopPane.addComponentListener(new DesktopPaneListener());
-        setContentPane(desktopPane);
-        getContentPane().setPreferredSize(
-                new Dimension(PANE_WIDTH, PANE_HEIGHT));
-
-        // create internal frames with size and position
-        menuFrame = new MenuFrame(PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, r, this, o);
-        orderFrame = new OrderFrame(PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, r, o);
-        kitchenFrame = new KitchenFrame(
-                PANE_WIDTH / 3, PANE_HEIGHT, PANE_WIDTH / 3, 0, r);
-        pickUpFrame = new PickUpFrame(
-                PANE_WIDTH/3, PANE_HEIGHT, PANE_WIDTH/3*2, 0, r);
-        
-        // attach listeners
-        r.getKitchen().attach(kitchenFrame);
-        r.getPickUpList().attach(pickUpFrame);
-        
-        // add internal frames to desktop pane
-        //desktopPane.add(orderFrame);
-        
-      
-        desktopPane.add(menuFrame);
-        desktopPane.add(kitchenFrame);
-        desktopPane.add(pickUpFrame);
-        
-        // set frame settings
-        setTitle("Meal Ordering Simulator");
-        setResizable(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-
-
-        setVisible(true);
-    }
-
-    
-    public void refreshOrderFrame() {
-    	desktopPane = new JDesktopPane();
-        desktopPane.addComponentListener(new DesktopPaneListener());
-        setContentPane(desktopPane);
-        getContentPane().setPreferredSize(
-                new Dimension(PANE_WIDTH, PANE_HEIGHT));
-
-        // create internal frames with size and position
-        orderFrame = new OrderFrame(
-                PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, r, o);
-        menuFrame = new MenuFrame(
-                PANE_WIDTH / 3, PANE_HEIGHT, 0, 0, r, this, o);
-        
-        kitchenFrame = new KitchenFrame(
-                PANE_WIDTH / 3, PANE_HEIGHT, PANE_WIDTH / 3, 0, r);
-        pickUpFrame = new PickUpFrame(
-                PANE_WIDTH/3, PANE_HEIGHT, PANE_WIDTH/3*2, 0, r);
-        
-        // attach listeners
-        r.getKitchen().attach(kitchenFrame);
-        r.getPickUpList().attach(pickUpFrame);
-        
-        // add internal frames to desktop pane
-        //desktopPane.add(orderFrame);
-        
-      
-        desktopPane.add(orderFrame);
-        desktopPane.add(kitchenFrame);
-        desktopPane.add(pickUpFrame);
-        
-        // set frame settings
-        setTitle("Meal Ordering Simulator");
-        setResizable(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setVisible(true);
-    }
-
-    
+    }   
     
     /**
-     * Custom component listener for desktop pane.
+     * Custom component listener for desktop pane to allow resizing.
      */
     private class DesktopPaneListener implements ComponentListener
     {
@@ -160,16 +67,14 @@ public class MainFrame extends JFrame
             PANE_HEIGHT = getContentPane().getHeight();
 
             // resize internal frames
-            orderFrame.setBounds(0, 0, PANE_WIDTH / 3, PANE_HEIGHT);
             menuFrame.setBounds(0, 0, PANE_WIDTH / 3, PANE_HEIGHT);
-            
-           
             kitchenFrame.setBounds(PANE_WIDTH / 3, 0, PANE_WIDTH / 3, PANE_HEIGHT);
             pickUpFrame.setBounds(PANE_WIDTH/3*2, 0, PANE_WIDTH/3, PANE_HEIGHT);
         }
 
         @Override
         public void componentMoved(ComponentEvent ce) {}
+        
         @Override
         public void componentShown(ComponentEvent ce) {}
 
