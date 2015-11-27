e741fc525839ac69350f13b530fecc797a740e68
Katherine Soohoo <katherinesoohoo@gmail.com>
2015-11-27 11:00:29 -0800

    added comments

--- src/Kitchen.java
+++ src/Kitchen.java
@@ -3,10 +3,17 @@ import datastructures.Queue;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+/**
+ * Kitchen as a queue of orders.
+ */
 public class Kitchen extends Queue<Order>
 {
     ChangeListener listener;
         
+    /**
+     * Adds an order.
+     * @param order order to add
+     */
     public void addOrder(Order order)
     {
         if (order != null)
@@ -16,6 +23,10 @@ public class Kitchen extends Queue<Order>
         }
     }
     
+    /**
+     * Removes an order
+     * @return order removed
+     */
     public Order removeOrder()
     {
         if (!isEmpty())
@@ -27,11 +38,18 @@ public class Kitchen extends Queue<Order>
         return null;
     }
     
+    /**
+     * Attaches change listener.
+     * @param c the listener
+     */
     public void attach(ChangeListener c)
     {
         listener = c;
     }
     
+    /**
+     * Updates change lister.
+     */
     public void update()
     {
         listener.stateChanged(new ChangeEvent(this));
